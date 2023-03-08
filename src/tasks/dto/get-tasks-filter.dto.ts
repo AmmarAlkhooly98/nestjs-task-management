5ee8cbc31ef874452ce175c25b2a7a453a662248
Ammar Alkhooly <ammaralkhooly1@gmail.com>
2023-03-09 00:45:50 +0300

    task DTOs added for filters status and task creations

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,13 +1,16 @@
 import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task-status.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GetTasksFilterDto {
   @IsOptional()
   @IsEnum(TaskStatus)
+  @ApiProperty({ required: false })
   status?: TaskStatus;
 
   @IsOptional()
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ required: false })
   search?: string;
 }
