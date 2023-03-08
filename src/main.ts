9743dce9304bf1ac5d2b4aacb49ac56b8c86b21c
Ammar Alkhooly <ammaralkhooly1@gmail.com>
2023-03-09 00:38:01 +0300

    added interceptor to root and updated docs

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,27 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { API_BASE_PATH } from './constants';
 import { WinstonLogger } from './utils/logger';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: WinstonLogger,
   });
-
   app.setGlobalPrefix(API_BASE_PATH);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   const config = new DocumentBuilder()
-    .setTitle('Vaccination API docs')
-    .setDescription('The vaccination API description')
+    .setTitle('Task Management API docs')
+    .setDescription('API for task management applications')
     .setVersion('1.0')
-    .addTag('persons')
+    .addTag('Auth')
+    .addTag('Tasks')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(`${API_BASE_PATH}/docs`, app, document);
 
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
 }
 bootstrap();
