2a176a0a6ef26ec28b4903d972707892c17e7341
Ammar Alkhooly <ammaralkhooly1@gmail.com>
2023-03-09 00:43:54 +0300

    jwt strategy auth setup

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(30)
+  @MinLength(3)
+  @ApiProperty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(200)
+  @MinLength(3)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak',
+  })
+  @ApiProperty()
+  password: string;
+}
